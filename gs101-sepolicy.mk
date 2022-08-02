09b82a158fd1a1c8d618ac8b05a55c1f971eb355
Michael Bestas <mkbestas@gmail.com>
2022-08-02 23:38:33 +0300

    Include missing vendor sepolicy
    Change-Id: Id35fb4b22e2ee8dfafdfb8ae601ab823435d2b94

--- gs101-sepolicy.mk
+++ gs101-sepolicy.mk
@@ -1,3 +1,11 @@
+# twoshay
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/input
+
+# google_battery service
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/googlebattery
+
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/connectivity_thermal_power_manager
+
 # sepolicy that are shared among devices using whitechapel
 BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/whitechapel/vendor/google
 
